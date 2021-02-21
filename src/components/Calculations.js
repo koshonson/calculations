f9e2f929d580c0e777c8775484bbbc964315512e
michaelKosatka <michael.kosatka@seznam.cz>
2021-02-21 20:06:31 +0100

    basic layout of Calculations component

--- src/components/Calculations.js
+++ src/components/Calculations.js
@@ -1,7 +1,30 @@
+import '../styles/calculations.css';
 import React from 'react';
 
-const Caulculations = () => {
-	return <h1>Caulculations</h1>;
+const Caulculations = ({ numProblems }) => {
+	const renderProblems = () => {
+		const result = [];
+		for (let i = 1; i <= numProblems; i++) {
+			result.push(
+				<div className="problem" key={i}>
+					81 / 9 = 9
+				</div>
+			);
+		}
+		return result;
+	};
+
+	return (
+		<div className="screen">
+			<div className="calculations-board">
+				<div className="problems">{renderProblems()}</div>
+				<div className="status-ribbon">
+					<div className="progress">PROGERSS</div>
+					<div className="timer">TIMER</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Caulculations;
