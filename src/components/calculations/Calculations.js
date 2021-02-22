5f554c3f10974b93078348c937be2d78f8850ef9
michaelKosatka <michael.kosatka@seznam.cz>
2021-02-22 19:55:21 +0100

    Timer (its operation/functionality) implemented

--- src/components/calculations/Calculations.js
+++ src/components/calculations/Calculations.js
@@ -1,17 +1,29 @@
 import '../../styles/calculations.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Problems from './Problems';
 import Progress from './Progress';
 import Timer from './Timer';
 
 const Caulculations = ({ numProblems }) => {
-	const [timer, setTimer] = useState(0);
+	const [time, setTime] = useState(0);
+	const [timer, setTimer] = useState(null);
 	const [progress, setProgress] = useState({
 		count: numProblems,
 		done: 0,
 		correct: 0,
 		wrong: 0
 	});
+
+	useEffect(() => {
+		const tickSec = () => setTime(time + 1);
+		const { count, done } = progress;
+		if (count !== done) {
+			setTimer(setTimeout(tickSec, 1000));
+		} else {
+			clearTimeout(timer);
+			setTimer(null);
+		}
+	}, [time, progress]);
 
 	const updateProgress = result => {
 		const update = { ...progress };
@@ -30,7 +42,7 @@ const Caulculations = ({ numProblems }) => {
 				/>
 				<div className="status-ribbon">
 					<Progress progress={progress} />
-					<Timer timer={timer} />
+					<Timer time={time} timer={timer} />
 				</div>
 			</div>
 		</div>
--- src/components/calculations/Timer.js
+++ src/components/calculations/Timer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { CgAlarm } from 'react-icons/cg';
 
-const Timer = () => {
+const Timer = ({ time, timer }) => {
 	return (
 		<div className="timer">
-			<div className="time">32s</div>
+			<div className="time">{time}s</div>
 			<CgAlarm
 				style={{ marginLeft: '0.25em', marginRight: '0.5em' }}
-				className="tick"
+				className={timer ? 'tick' : null}
 			/>
 		</div>
 	);
