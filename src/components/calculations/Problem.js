437b7f36fce44976eeac5907f1efd3cd75fa8640
michaelKosatka <michael.kosatka@seznam.cz>
2021-02-22 15:42:55 +0100

    Problem component level state and  UI/navigation implemented

--- src/components/calculations/Problem.js
+++ src/components/calculations/Problem.js
@@ -1,16 +1,77 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-const Problem = () => {
+const Problem = ({ id }) => {
+	const [result, setResult] = useState(8);
+	const [input, setInput] = useState('');
+	const [submitted, setSubmitted] = useState(false);
+	const [correct, setCorrect] = useState(null);
+
+	const container = useRef();
+
+	const focusNext = id => {
+		const next = document.getElementById(+id + 1);
+		if (next) {
+			next.focus();
+		} else {
+			document.getElementById(1).focus();
+		}
+	};
+
+	const handleChange = ({ target }) => {
+		if (
+			!submitted &&
+			target.value !== ' ' &&
+			(+target.value || +target.value === 0)
+		) {
+			setInput(parseInt(target.value.slice(0, 3)));
+		}
+	};
+
+	const handleEnterSubmit = ({ key, target }) => {
+		if (key !== 'Enter') return;
+		target.blur();
+		focusNext(target.id);
+	};
+
+	const handleBlurSubmit = () => {
+		container.current.classList.remove('focused');
+		if (input || input === 0) {
+			setSubmitted(true);
+			setCorrect(result === input);
+			// updateProgress(result === input);
+		}
+	};
+
+	const handleFocus = ({ target }) => {
+		container.current.classList.add('focused');
+		if (submitted) focusNext(target.id);
+	};
+
+	const getDivClassnames = () => {
+		let baseClassnames = 'problem centered';
+		if (submitted) {
+			return (baseClassnames += correct ? ' correct' : ' wrong');
+		}
+		return baseClassnames;
+	};
+
+	const getInputClassnames = () => {
+		return !submitted ? 'problem-result' : 'problem-result locked';
+	};
+
 	return (
-		<div className="problem centered">
+		<div className={getDivClassnames()} ref={container}>
 			<span className="problem-expression">81 / 9 = </span>
-			<span
-				className="problem-result"
-				contentEditable
-				suppressContentEditableWarning
-			>
-				9
-			</span>
+			<input
+				className={getInputClassnames()}
+				type="text"
+				id={id}
+				value={input}
+				onChange={e => handleChange(e)}
+				onKeyUp={e => handleEnterSubmit(e)}
+				onBlur={e => handleBlurSubmit(e)}
+				onFocus={e => handleFocus(e)}
+			></input>
 		</div>
 	);
 };
--- src/components/calculations/Problems.js
+++ src/components/calculations/Problems.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Problem from './Problem';
 
-const Problems = ({ numProblems }) => {
+const Problems = ({ numProblems, updateProgress }) => {
+	useEffect(() => {
+		document.getElementById(1).focus();
+	}, []);
+
 	const renderProblems = () => {
 		const result = [];
 		for (let i = 1; i <= numProblems; i++) {
-			result.push(<Problem key={i} />);
+			result.push(<Problem key={i} id={i} updateProgress={updateProgress} />);
 		}
 		return result;
 	};
