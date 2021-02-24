718faccaf077cd40eb2204bae55a12920675b498
michaelKosatka <michael.kosatka@seznam.cz>
2021-02-24 15:20:40 +0100

    re-styled navbar & Home screen, added +/- btns to num input on Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import '../styles/home.css';
 import React, { useEffect } from 'react';
+import { VscSymbolOperator } from 'react-icons/vsc';
 
 const Home = ({ numProblems, updateNumProblems, startGame }) => {
 	useEffect(() => {
@@ -17,15 +18,30 @@ const Home = ({ numProblems, updateNumProblems, startGame }) => {
 	return (
 		<div className="screen centered">
 			<div className="controls">
+				<VscSymbolOperator className="home-icon" />
 				<h3>Number of Problems:</h3>
-				<input
-					className="number-input"
-					type="number"
-					step="1"
-					value={numProblems}
-					onChange={e => updateNumProblems(+e.target.value)}
-					autoFocus
-				></input>
+				<div className="number-input-container">
+					<button
+						className="btn-input rounded-left"
+						onClick={() => updateNumProblems(numProblems - 1)}
+					>
+						-
+					</button>
+					<input
+						className="number-input"
+						type="number"
+						step="1"
+						value={numProblems}
+						onChange={e => updateNumProblems(+e.target.value)}
+						autoFocus
+					></input>
+					<button
+						className="btn-input rounded-right"
+						onClick={() => updateNumProblems(numProblems + 1)}
+					>
+						+
+					</button>
+				</div>
 				<button className="btn" onClick={startGame}>
 					START
 				</button>
